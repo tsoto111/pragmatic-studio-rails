b5282691f7f7ab74aa3ffaf417d0e5a9a49a14df
Tavo <tsoto111@gmail.com>
2018-07-09 14:31:43 -0500

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #verb "url" => "name_of_controller#name_of_action"
+  get "movies" => "movies#index"
+  
+end
