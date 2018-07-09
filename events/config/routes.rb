3e6b82ed5c23b833bfd31579147f265eef009162
Tavo <tsoto111@gmail.com>
2018-07-09 16:12:25 -0500

    Lesson 8 Routes, adding a details page.

--- events/config/routes.rb
+++ events/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get "events" => "events#index"
+  get "events/:id" => "events#show"
   
 end
