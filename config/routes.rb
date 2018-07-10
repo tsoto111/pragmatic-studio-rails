171267ce3b1c35a6d22b9224df5fb683a7b8d667
Tavo <tsoto111@gmail.com>
2018-07-10 14:22:33 -0500

    Lesson 11 Edit view and form

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root "movies#index"
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit"
   
 end
