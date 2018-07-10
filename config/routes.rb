0981daf6e5266001f030908f9db8bf667ec434fe
Tavo <tsoto111@gmail.com>
2018-07-10 11:28:00 -0500

    Lesson 9 Routes and linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #verb "url" => "name_of_controller#name_of_action"
+  root "movies#index"
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: "movie"
   
 end
