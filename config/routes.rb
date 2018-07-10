03ed998afeef8783ad0488f80f4143736fc4f367
Tavo <tsoto111@gmail.com>
2018-07-10 13:10:30 -0500

    Lesson 11 Forms and Editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
+  
+  # Event Paths
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event"
+  get "events/:id/edit" => "events#edit", as: "edit_event"
   
 end
