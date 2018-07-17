21334ecbfda643a479bcdae7439350507d1db595
Tavo <tsoto111@gmail.com>
2018-07-17 12:43:52 -0500

    Lesson 12 Creating with forms.

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -1,7 +1,18 @@
 class EventsController < ApplicationController
+	
 	def index
 		@events = Event.all
 	end	
+	
+	def new
+		@event = Event.new
+	end
+	
+	def create
+		@event = Event.new(event_params)
+		@event.save
+		redirect_to @event
+	end
 	
 	def show
 		@event = Event.find(params[:id])
@@ -13,9 +24,15 @@ class EventsController < ApplicationController
 	
 	def update
 		@event = Event.find(params[:id])
-		@event_params = params.require(:event).permit(:name, :description, :location, :price, :start_at)
-		@event.update(@event_params)
+		@event.update(event_params)
 		redirect_to @event
+	end
+	
+	
+private
+
+	def event_params
+		params.require(:event).permit(:name, :description, :location, :price, :starts_at)
 	end
 	
 end
--- events/config/routes.rb
+++ events/config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root "events#index"
   
   # Event Paths
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  #get "events" => "events#index"
+  #get "events/new" => "events#new"
+  #get "events/:id" => "events#show", as: "event"
+  #get "events/:id/edit" => "events#edit", as: "edit_event"
+  #patch "events/:id" => "events#update"
+  resources :events
   
 end
