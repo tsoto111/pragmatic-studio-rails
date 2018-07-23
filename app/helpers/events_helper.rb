484ca2bff1e882e793c426ac8a10811239dfda31
Tavo <tsoto111@gmail.com>
2018-07-23 11:37:15 -0500

    Lesson 16 Migrations Revisited, adding images and capacity to events.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,13 @@ module EventsHelper
 			number_to_currency(event.price)
 		end
 	end
+	
+	def image_for(event)
+		if event.image_file_name.blank?
+			image_tag 'placeholder.png', :style => "width:100px; height:auto; display:block;"
+		else
+			image_tag event.image_file_name, :style => "width:100px; height:auto; display:block;"
+		end
+	end
+	
 end
