bf42b77e76ea155abea5fe323924e7cc24bf4eb9
Tavo Soto <tavo@newmedio.com>
2018-08-27 16:53:22 -0500

    Fixing my mess up.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180723164819) do
+ActiveRecord::Schema.define(version: 20160526164749) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
