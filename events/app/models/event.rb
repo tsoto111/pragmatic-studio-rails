7de0b6ad0839d65ba80532afd920d4e703e2885a
Tavo Soto <tavo@newmedio.com>
2018-08-27 17:03:20 -0500

    Adding Event and Flix redesign of rails apps.

--- events/app/models/event.rb
+++ events/app/models/event.rb
@@ -1,10 +1,9 @@
 class Event < ApplicationRecord
-	def free?
-		price.blank? || price.zero?
-	end
-	
-	def self.upcoming
-		where("starts_at >= ?",Time.now).order("starts_at")
-	end
-	
+  def self.upcoming
+    where('starts_at >= ?', Time.now).order(:starts_at)
+  end
+
+  def free?
+    price.blank? || price.zero?
+  end
 end
---
+++ events_before_redesign/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "events#index"
+  
+  # Event Paths
+  #get "events" => "events#index"
+  #get "events/new" => "events#new"
+  #get "events/:id" => "events#show", as: "event"
+  #get "events/:id/edit" => "events#edit", as: "edit_event"
+  #patch "events/:id" => "events#update"
+  resources :events
+  
+end
