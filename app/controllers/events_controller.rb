c5fd014424c53df1892c4a79a24577c7eec65164
Tavo Soto <tavo@newmedio.com>
2018-08-28 12:41:56 -0500

    Lesson 19 Validations Part II Video

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,43 +1,51 @@
 class EventsController < ApplicationController
-  def index
-    @events = Event.upcoming
-  end
+    def index
+        @events = Event.upcoming
+    end
 
-  def show
-    @event = Event.find(params[:id])
-  end
+    def show
+        @event = Event.find(params[:id])
+    end
 
-  def edit
-    @event = Event.find(params[:id])
-  end
+    def edit
+        @event = Event.find(params[:id])
+    end
 
-  def update
-    @event = Event.find(params[:id])
-    @event.update(event_params)
-    redirect_to @event
-  end
+    def update
+        @event = Event.find(params[:id])
+        @event.update(event_params)
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            render :edit
+        end
+    end
 
-  def new
-    @event = Event.new
-  end
+    def new
+        @event = Event.new
+    end
 
-  def create
-    @event = Event.new(event_params)
-    @event.save
-    redirect_to @event
-  end
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            redirect_to @event
+        else
+            render :new
+        end
+    
+    end
 
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to events_url
-  end
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+        redirect_to events_url
+    end
 
-private
+    private
 
-  def event_params
-    params.require(:event).
-      permit(:name, :description, :location, :price, :starts_at, :image_file_name, :capacity)
-  end
+    def event_params
+        params.require(:event).
+        permit(:name, :description, :location, :price, :starts_at, :image_file_name, :capacity)
+    end
 
 end
