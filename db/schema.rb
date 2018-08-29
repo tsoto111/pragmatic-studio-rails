18819ffc062968a142308450689c6fad0c82c044
Tavo Soto <tavo@newmedio.com>
2018-08-29 12:45:58 -0500

    Lesson 21 One-to-Many Models Exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526164749) do
+ActiveRecord::Schema.define(version: 20180829172714) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20160526164749) do
     t.string   "director"
     t.string   "duration"
     t.string   "image_file_name", default: ""
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "stars"
+    t.text     "comment"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
 end
