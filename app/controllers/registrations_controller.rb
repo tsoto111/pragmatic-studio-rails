400c54ede527d175d67025c21c15fbb3402d634f
Tavo Soto <gsoto@itx.com>
2019-01-28 15:31:02 -0600

    Lesson 23 One-to-Many Forms Video

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,32 @@
 class RegistrationsController < ApplicationController
+
+    before_action :set_up
+
     def index
-        @event = Event.find(params[:event_id])
         @registrations = @event.registrations
     end
+
+    def new
+        @registration = @event.registrations.new
+    end
+
+    def create
+        @registration = @event.registrations.new(registration_params)
+        if @registration.save
+            redirect_to event_registrations_path(@event), notice: "Thanks, you are registered!"
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def set_up
+        @event = Event.find(params[:event_id])
+    end
+
+    def registration_params
+        params.require(:registration).permit(:name, :email,:how_heard)
+    end
+
 end
