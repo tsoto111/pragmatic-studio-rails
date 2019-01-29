731e5c9717ec5fe05b5c3e46f093a07b7cfdb05f
Tavo Soto <gsoto@itx.com>
2019-01-29 10:29:39 -0600

    Lesson 24 One-to-Many Logic Exercise

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -32,4 +32,9 @@ class Movie < ApplicationRecord
     def flop?
         total_gross.blank? || total_gross < 50000000
     end
+
+    def average_stars
+        self.reviews.average(:stars) 
+    end
+
 end
