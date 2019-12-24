a2b6cd86ee51542232bc4da9645de77b9b6942ca
Tavo Soto <tavo@newmedio.com>
2019-12-24 12:22:17 -0600

    Lesson 5 Excercise: Sign out logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,5 +12,11 @@ class SessionsController < ApplicationController
 			render :new
 		end
 	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, notice: "You're now signed out!"
+	end
+
 end
 
