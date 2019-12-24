0e0b28fd9dbf9671004fc420796e0ca9b3888673
Tavo Soto <tavo@newmedio.com>
2019-12-24 14:57:05 -0600

    Lesson 6 Part I: Excercise over required signing logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_action :require_signin, except: [:new, :create]
+
 	def index
 		@users = User.all
 	end
@@ -15,7 +17,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = user.id
+			session[:user_id] = @user.id
 			redirect_to @user, notice: "Thanks for signing up!"
 		else
 			render :new
