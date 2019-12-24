6c2e69d99b956f51e0037a54232fa0780db97e4d
Tavo Soto <tavo@newmedio.com>
2019-12-24 10:07:37 -0600

    Lesson 4 Part II Exercise: adding user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		fail
+		if user = User.authenticate(params[:email], params[:password])
+			session[:user_id] = user.id
+			flash[:notice] = "Welcome back, #{user.name}!"
+			redirect_to user
+		else
+			flash.now[:alert] = "Invalid email/password combination!"
+			render :new
+		end
 	end
-
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,9 @@ class User < ApplicationRecord
 		uniqueness: { 
 			case_sensitive: false 
 		}
-
+	
+	def self.authenticate(email, password)
+		user = User.find_by(email: email)
+		user && user.authenticate(password)
+	end
 end
